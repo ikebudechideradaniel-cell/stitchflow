288fab4e82ea2d6a6c344bcd39463eb5c285d700
I.C.DANIEL MONEY $$ <ikebudechideradaniel@gmail.com>
2025-11-18 06:16:31 -0800

    test branch setup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,33 +1,22 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { _View, StyleSheet } from 'react-native';
+import { View,Text } from 'react-native';
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+
+
 
 export default function HomeScreen() {
   return (
-<></>
+  <View style={styles.wrapper}>
+    <Text>DAY 2 IN REACT NATIVE</Text>
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+wrapper:{
+  flex : 1,
+  backgroundColor:"red",
+  alignItems:"center",
+  justifyContent:'center'
+}
 });
